b54a509c363ccc163b3cd4d8b292347f51b7b0d3
NathanSkoczylas <nathan_skoczylas@hotmail.com>
2023-07-12 05:52:27 -0300

    Set up RayLib

--- code/endless_runner.cpp
+++ code/endless_runner.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#include "raylib.h"
+
 
 // ----------------------------------------------------------------------------
 // main
@@ -7,6 +9,23 @@
 int main() {
 	std::cout << "Endless Runner" << std::endl;
 	
+	// Window Parameters
+	int windowWidth{ 800 };
+	int windowHeight{ 450 };
+	InitWindow(windowWidth, windowHeight, "Endless Runner");
+	Color backgroundColor{ WHITE };
+	SetTargetFPS(60);
+
+	// Game loop
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+
+		ClearBackground(backgroundColor);
+
+		EndDrawing();
+	}
+
+	CloseWindow();
 	
 	return 0;
 }
